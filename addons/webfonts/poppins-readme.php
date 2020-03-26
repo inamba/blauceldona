e1df619722f816add3fbdce33ced14be1ee8ac7e
inamba <62590806+inamba+macbook@users.noreply.github.com>
2020-03-26 03:56:00 -0300

    Some files

---
+++ addons/webfonts/poppins-readme.php
@@ -0,0 +1,40 @@
+<?php
+	
+	/*
+	
+	@import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,700');
+	
+	# ---------- latin-ext
+	
+	@font-face {
+	  font-family: 'Poppins';
+	  font-style: normal;
+	  font-weight: 300;
+	  src: local('Poppins Light'), local('Poppins-Light'), url(https://fonts.gstatic.com/s/poppins/v5/pxiByp8kv8JHgFVrLDz8Z1JlFd2JQEl8qw.woff2) format('woff2');
+	  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+	}
+	@font-face {
+	  font-family: 'Poppins';
+	  font-style: normal;
+	  font-weight: 400;
+	  src: local('Poppins Regular'), local('Poppins-Regular'), url(https://fonts.gstatic.com/s/poppins/v5/pxiEyp8kv8JHgFVrJJnecnFHGPezSQ.woff2) format('woff2');
+	  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+	}
+	@font-face {
+	  font-family: 'Poppins';
+	  font-style: normal;
+	  font-weight: 500;
+	  src: local('Poppins Medium'), local('Poppins-Medium'), url(https://fonts.gstatic.com/s/poppins/v5/pxiByp8kv8JHgFVrLGT9Z1JlFd2JQEl8qw.woff2) format('woff2');
+	  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+	}
+	@font-face {
+	  font-family: 'Poppins';
+	  font-style: normal;
+	  font-weight: 700;
+	  src: local('Poppins Bold'), local('Poppins-Bold'), url(https://fonts.gstatic.com/s/poppins/v5/pxiByp8kv8JHgFVrLCz7Z1JlFd2JQEl8qw.woff2) format('woff2');
+	  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+	}
+	
+	*/
+	
+	exit;
